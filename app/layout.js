3a9c88cf5bb2d781c72b152dec2f05988bcad034
Nezarabouhamdan <nwa200079@gmail.com>
2025-06-24 11:52:25 +0400

    Version 1.0.0 GTM

--- app/layout.js
+++ app/layout.js
@@ -81,6 +81,20 @@ export default function RootLayout({ children }) {
       `,
           }}
         />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TNHGBH7J');
+    `,
+          }}
+        />
+
         <noscript>
           <img
             height="1"
@@ -92,7 +106,14 @@ export default function RootLayout({ children }) {
       </head>
 
       <body>
-        {" "}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TNHGBH7J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ScrollToTop />
         <StyledComponentsRegistry>
           <ClientProviders>
