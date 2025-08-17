4d9adccc2da30fb705b9725c594c12d8ca8ecb0d
Nezarabouhamdan <nwa200079@gmail.com>
2025-08-17 16:42:33 +0400

    `Added Google Tag Manager script to LandingLayout component`

--- app/[lang]/(landing)/landing/layout.js
+++ app/[lang]/(landing)/landing/layout.js
@@ -31,6 +31,15 @@ export default function LandingLayout({ children, params: { lang } }) {
         gtag('js', new Date());
         gtag('config', 'G-FYYEQCM8Z7');
       `}
+        </Script>{" "}
+        <Script id="google-tag-manager-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TNHGBH7J');
+          `}
         </Script>
         {/* Google Tag Manager */}
         <Script
