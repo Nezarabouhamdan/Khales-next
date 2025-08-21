cb0386fe04a50cd308f8efec96d1e9b53dd160e6
Nezarabouhamdan <nwa200079@gmail.com>
2025-08-21 10:21:10 +0400

    `Added Facebook Meta Pixel scripts to landing and main layouts`

--- app/[lang]/(landing)/landing/layout.js
+++ app/[lang]/(landing)/landing/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
   title: "Khales Landing Page",
   description: "Special offer from Khales Group.",
 };
+const META_PIXEL_ID = "3634194126882623"; // Replace with your actual pixel ID
 
 // This is a self-contained Root Layout for the landing page group.
 export default function LandingLayout({ children, params: { lang } }) {
@@ -55,6 +56,24 @@ export default function LandingLayout({ children, params: { lang } }) {
           `}
         </Script>{" "}
          */}
+        <Script
+          id="facebook-meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+        n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,\'script\',
+        \'https://connect.facebook.net/en_US/fbevents.js\' );
+        fbq(\'init\', \'${META_PIXEL_ID}\');
+        fbq(\'track\', \'PageView\');
+      `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <StyledComponentsRegistry>
--- app/[lang]/(main)/layout.js
+++ app/[lang]/(main)/layout.js
@@ -79,6 +79,7 @@ export async function generateMetadata({ params: { lang } }) {
     },
   };
 }
+const META_PIXEL_ID = "3634194126882623"; // Replace with your actual pixel ID
 
 export default async function RootLayout({ children, params }) {
   const lang = params.lang || i18n.defaultLocale;
@@ -153,6 +154,24 @@ export default async function RootLayout({ children, params }) {
         />
         {/* ====================================================== */}
         <LocalBusinessSchema />
+        <Script
+          id="facebook-meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+        n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,\'script\',
+        \'https://connect.facebook.net/en_US/fbevents.js\' );
+        fbq(\'init\', \'${META_PIXEL_ID}\');
+        fbq(\'track\', \'PageView\');
+      `,
+          }}
+        />
       </head>
       <body>
         <noscript>
